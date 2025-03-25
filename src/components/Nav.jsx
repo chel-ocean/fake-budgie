119eda1d4a6f9db655d0d72629ac512fbf200805
Chelsea <chelsea.yang8@gmail.com>
2025-03-25 04:37:17 -0600

    delete comments

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,26 +6,24 @@ import {TrashIcon, HomeIcon} from '@heroicons/react/20/solid'
 const Nav = ({userName}) => {
     return (
         <nav>
-            {/* <div className="flex items-center justify-evenly p-4"> */}
-                <NavLink to="/" aria-label="Go to Home">
-                    <HomeIcon width={25} />
-                    <span className="text-4xl">Budgie</span>
-                </NavLink>
-                {
-                    userName && (
-                        <Form method="post" action="/logout" onSubmit={(event) => {
-                            if (!confirm("Delete all user data?")) {
-                                event.preventDefault() // prevents refresh on submit
-                            }
-                        }}>
-                            <button type="submit" className="btn btn--warning">
-                                <span>Delete User</span>
-                                <TrashIcon width={25} />
-                            </button>
-                        </Form>
-                    )
-                }
-            {/* </div> */}
+            <NavLink to="/" aria-label="Go to Home">
+                <HomeIcon width={25} />
+                <span className="text-4xl">Budgie</span>
+            </NavLink>
+            {
+                userName && (
+                    <Form method="post" action="/logout" onSubmit={(event) => {
+                        if (!confirm("Delete all user data?")) {
+                            event.preventDefault() // prevents refresh on submit
+                        }
+                    }}>
+                        <button type="submit" className="btn btn--warning">
+                            <span>Delete User</span>
+                            <TrashIcon width={25} />
+                        </button>
+                    </Form>
+                )
+            }
         </nav>
     )
 }
